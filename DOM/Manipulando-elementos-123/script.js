2aada1e529205d10405dad63bd909ec2f642ba39
Pedro Henrique <pedrofuturama620@gmail.com>
2024-07-30 19:59:34 -0300

    DOM -- Manipulando objetos 1

--- DOM/Manipulando-elementos-123/script.js
+++ DOM/Manipulando-elementos-123/script.js
@@ -1,37 +1,12 @@
-
-//Aula 1
-/*
 function clicou(){
 	const teste = document.querySelector("#teste1");
 	const ul = document.querySelector("ul");
-	ul.innerHTML = "<li>Item Alterado</li>";
-	ul.innerHTML = ul.innerHTML + "<li>Item Alterado</li>";
+	//ul.innerHTML = "<li>Item Alterado</li>"
+	ul.innerHTML = ul.innerHTML + "<li>Item Alterado</li>"
 	//console.log(teste.children);
-	//console.log(teste.children[0].children);
 }
 
 function alterarDiv2(){
-	const div2 = document.querySelector("#teste2");
-	const elemento2 = document.querySelector("#ul2");
-	elemento2.innerHTML = elemento2.innerHTML + "<li>Alguma coisa 6</li>";
-
-	console.log(div2.children);
-	console.log(elemento2.children[0]);
-
-
-}
-
-*/
-//Manipulando elementos 2
-
-function clicou(){
-	const teste = document.querySelector("#teste1");
-	const ul = document.querySelector("ul");
-	
-	ul.children[0].append(" Alterado");
-
-	let newElement = document.createElement("li");
-	newElement.innerText = "Algum item ";
-
-	ul.appendChild(newElement);
+	var teste2 = document.querySelector("#teste2");
+	console.log(teste2.innerHTML += " trato feito ");
 }
