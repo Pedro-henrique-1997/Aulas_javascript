0984fd8988377d4bb41901f8c864eb41873e66ca
Pedro Henrique <pedrofuturama620@gmail.com>
2024-08-12 20:36:18 -0300

    Manipulando CSS

---
+++ DOM/Manipulando CSS/script.js
@@ -0,0 +1,11 @@
+function clicou(){
+	const div = document.querySelector("#teste1");
+	div.style.backgroundColor = "blue";
+	div.style.color = "white";
+}
+
+function alterarDiv2(){
+	const div2 = document.querySelector("#teste2");
+	div2.style.backgroundColor = "yellow";
+	div2.style.color = "red";
+}
